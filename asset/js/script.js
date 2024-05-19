61a5c2458efe1667878d93e880f095da43e81b2d
Md. Anower Hossan <anowerhossan1988@gmail.com>
2024-05-19 20:01:53 +0600

    Design done for desktop dersion

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-
   // project modal
   $(".project_img").click(function () {
     let imageId = $(this).attr("id");
@@ -15,7 +14,52 @@ $(document).ready(function(){
   });
 
   // Project modal close
-  $(".modal_close").click(function(){
-    $(this).closest(".project_modal_main").css({transform: "scale(0)"});
+  $(".modal_close").click(function () {
+    $(this).closest(".project_modal_main").css({ transform: "scale(0)" });
   });
+
+  // Back to top
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $(".back_to_top").css({ transform: "translateY(0)" });
+    } else {
+      $(".back_to_top").css({ transform: "translateY(70px)" });
+    }
+  });
+
+  // fixed menubar
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 30) {
+      $(".header_section").css({
+        position: "fixed",
+        top: "0",
+        left: "0",
+        right: "0",
+        with: "100%",
+        background: "var(--black)",
+        zIndex: "999999999",
+      });
+      $(".slider_section").css({ marginTop: "80px" });
+    } else {
+      $(".header_section").css({
+        position: "unset",
+        background: "var(--black1)",
+      });
+      $(".slider_section").css({ marginTop: "0" });
+    }
+  });
+
+  // Menubar active class change
+  $(".menu_link").click(function () {
+    $(".menu_link").removeClass("active");
+    $(this).addClass("active");
+  });
+
+  // Slider change start here
+  // Slidr Change next
+  $(".slide_next").click(function(){
+    let sliderParent = $(this).closest(".testimonial_body");
+    let allSlider = sliderParent.find(".feedback_inner");
+  });
+  // Slider change end here
 });
